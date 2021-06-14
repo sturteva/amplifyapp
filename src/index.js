23dd9acceef84de562afff77c3e8f0aac0cb6840
sturteva <32139677+sturteva@users.noreply.github.com>
2021-06-14 16:10:28 -0500

    reset #2

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
-
 Amplify.configure(config);
 
 ReactDOM.render(
